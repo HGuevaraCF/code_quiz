0b938284d409bb951a2e727e158c389f505a0b13
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-03-27 21:56:00 -0600

    buttons hide/show behaviour

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,3 +5,33 @@ var secondQuestion = $('#page_2');
 var thirdQuestion = $('#page_3');
 var SaveScore = $('#saveScore');
 var ActualScore = $('#ActualScore');
+
+var scores = [];
+var scoreCount = 0;
+
+startButton.on('click', function(){
+    Introduction.hide();
+    fisrtQuestion.show();
+})
+
+$('.FirstQuestionButton').on('click', function(){
+    fisrtQuestion.hide();
+    secondQuestion.show();
+
+})
+
+$('.SecondOptnButton').on('click', function(){
+    secondQuestion.hide();
+    thirdQuestion.show();
+})
+
+$('.ThirdOptnButton').on('click', function(){
+    thirdQuestion.hide();
+    SaveScore.show();
+})
+
+$('#submitBtn').on('click', function(event){
+    event.preventDefault();
+    localStorage.setItem("Actual Score", ActualScore.val());
+
+})
