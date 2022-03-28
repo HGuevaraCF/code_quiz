4bb5d180bfb8fd771649c4ed3ce9d4a30eadacae
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-03-27 21:55:25 -0600

    script variable assign

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+var startButton = $('#start_button');
+var Introduction = $('#page_0');
+var fisrtQuestion = $('#page_1');
+var secondQuestion = $('#page_2');
+var thirdQuestion = $('#page_3');
+var SaveScore = $('#saveScore');
+var ActualScore = $('#ActualScore');
